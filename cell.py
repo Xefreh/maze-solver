02b981fdf4da41bf27d6eacab5c91ac7dc67f754
Xefreh <william.strainchamps@gmail.com>
2024-03-12 01:55:19 +0100

    inital commit

---
+++ cell.py
@@ -0,0 +1,81 @@
+from graphics import Line, Point
+
+
+class Cell:
+    def __init__(self, win=None):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+        self.visited = False
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
+
+    def draw_move(self, to_ceil, undo=False):
+        if self._win is None:
+            return
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        to_x_mid = (to_ceil._x1 + to_ceil._x2) / 2
+        to_y_mid = (to_ceil._y1 + to_ceil._y2) / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        if self._x1 > to_ceil._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_ceil._x2, to_y_mid))
+            self._win.draw_line(line, fill_color)
+        elif self._x1 < to_ceil._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_ceil._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+        elif self._y1 > to_ceil._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_ceil._y2), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+        elif self._y1 < to_ceil._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_ceil._y1))
+            self._win.draw_line(line, fill_color)
